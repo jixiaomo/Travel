a8fc0238edcb0ff1550520e429d3421a02446433
jixiaomo <2030023229@qq.com>
2018-04-27 09:48:18 +0800

    add icons

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="wrapper">
-    <swiper :option="swiperOption" >
+    <swiper :options="swiperOption">
       <swiper-slide v-for="item of swiperList" :key="item.id">
         <img class="swiper-img" :src="item.imgUrl" />
       </swiper-slide>
-      <div class="swiper-pagination" slot="pagination"></div>
+      <div class="swiper-pagination"  slot="pagination"></div>
     </swiper>
   </div>
 </template>
@@ -32,11 +32,11 @@ export default {
 
 <style lang="stylus" scoped>
   .wrapper >>> .swiper-pagination-bullet-active
-    background: #f00
+    background: #fff
   .wrapper
+    overflow: hidden
     width: 100%
     height: 0
-    overflow: hidden
     padding-bottom: 31.25%
     background: #eee
     .swiper-img
