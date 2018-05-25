2efb84e3500b06d4ea744ba8a9d5c2fa10a968a2
jixiaomo <2030023229@qq.com>
2018-05-25 09:22:47 +0800

    finish pages

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -8,19 +8,18 @@
       输入城市/景点/游玩主题</div>
     <router-link to='/city'>
       <div class="header-right">
-        {{this.doubleCity}}
+        {{this.city}}
         <span class="iconfont arrow-icon"></span>
       </div>
     </router-link>
   </div>
 </template>
 <script>
-import { mapState, mapGetters } from 'vuex'
+import { mapState } from 'vuex'
 export default {
   name: 'HomeHeader',
   computed: {
-    ...mapState(['city']),
-    ...mapGetters(['doubleCity'])
+    ...mapState(['city'])
   }
 }
 </script>
